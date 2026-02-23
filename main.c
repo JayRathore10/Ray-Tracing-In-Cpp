23c28da15faddc8b8511584583db700649ff2e12
Jay <jayrathore88155@gmail.com>
2026-02-23 22:46:01 +0530

    Create our first window

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include<SDL2/SDL.h>
 
+#define WIDTH 1200
+#define HEIGHT 400
+
 int main(int argc  , char** args){
+  SDL_Window* window = SDL_CreateWindow("Ray Tracing", SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , WIDTH , HEIGHT, 0);
+
+  SDL_Delay(3000);
+
   return 0;
 }
